b9cea5dfc2d40ff3b6c62fde2e40121134b7f7b3
Viktor Deilert <Viktor.deilert@gmail.com>
2025-09-16 16:54:50 +0200

    Removed redundant folder and files

--- hr_analytics_dbt/models/mart/mart_business.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='view') }}
-
-select *
-from {{ ref('fact_job_ads') }}
-where occupation_field_id = 'NYW6_mP6_vwf'
-
--- hr_analytics_dbt/models/mart/mart_health.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='view') }}
-
-select *
-from {{ ref('fact_job_ads') }}
-where occupation_field_id = 'MVqp_eS8_kDZ'
-
